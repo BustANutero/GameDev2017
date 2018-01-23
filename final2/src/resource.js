acf291d36dcb9d8138b169f1219702c8cc019629
Vansh Sandhu <32467894+BustANutero@users.noreply.github.com>
2018-01-23 00:52:08 -0800

    Add files via upload

---
+++ final2/src/resource.js
@@ -0,0 +1,16 @@
+/*******************************************************
+Copyright (C) 2016 meiry242@gmail.com
+This file is part of http://www.gamedevcraft.com .
+Author : Meir yanovich
+********************************************************/ 
+var res = {
+    Blank_png : "res/Images/Blank.png",
+    Block_png : "res/Images/block_80_180.png",
+    Circle_png : "res/Images/circle_30.png",
+    Gem_png : "res/Images/gem_30.png",
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
